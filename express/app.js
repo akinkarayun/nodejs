36596162ea92a779d7c77615d972b2bb5aa2633b
Akin Karayun <akinkarayun1@hotmail.com>
2022-09-01 13:15:57 +0300

    created simple server to show user simple pages

--- express/app.js
+++ express/app.js
@@ -1,8 +1,32 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server")
-  res.end("home page")
+  console.log(req.method)
+  //basic header
+  const url = req.url
+  if (url === "/") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    })
+    res.write("<h1>Home Page</h1>")
+    res.end()
+  }
+  //about page
+  else if (url === "/about") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    })
+    res.write("<h1>About Page</h1>")
+    res.end()
+  }
+  //404
+  else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    })
+    res.write("<h1>page not found</h1>")
+    res.end()
+  }
 })
 
 server.listen(5000)
