c99cf76696bcd2446086b132a5ad252c1706012b
Akin Karayun <akinkarayun1@hotmail.com>
2022-09-01 15:37:24 +0300

    learned how middleware works

--- express/app.js
+++ express/app.js
@@ -1,60 +1,23 @@
 const express = require("express")
 const app = express()
 
-const { products } = require("./data")
+// req => middleware => res
 
-app.get("/", (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  // res.send('Testing')
+  console.log(method, url, time)
+  next()
+}
+
+app.get("/", logger, (req, res) => {
+  res.send("Home")
 })
 
-app.get("/api/products", (req, res) => {
-  const newProduct = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
-  res.json(newProduct)
-})
-
-app.get("/api/products/:productId", (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productId } = req.params
-  const singleProduct = products.find(
-    (product) => product.id === Number(productId)
-  )
-  //if product === undefined
-  if (!singleProduct) {
-    return res.status(404).send("Product Does Not Exist")
-  }
-  res.json(singleProduct)
-})
-
-app.get("/api/products/:productId/reviews/:reviewId", (req, res) => {
-  console.log(req)
-  console.log(req.params)
-  res.send("Hello World")
-})
-
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.includes(search)
-    })
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send("No Product Exist")
-    return res.status(200).json({ success: true, data: [] })
-  }
-  res.status(200).json(sortedProducts)
-  // console.log("Hello World")
+app.get("/about", logger, (req, res) => {
+  res.send("About")
 })
 
 app.listen(5000, () => {
